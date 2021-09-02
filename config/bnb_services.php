d330fc6438a07a2d8b9eec285e5b7edcfbe83695
marcocanopoli <canopoli.marco@gmail.com>
2021-09-02 14:52:06 +0200

    add: services icons

---
+++ config/bnb_services.php
@@ -0,0 +1,84 @@
+<?php 
+
+return [
+    [
+        'name' => 'Aria condizionata',
+        'icon' => 'aria-condizionata.svg'
+    ],
+    [
+        'name' => 'Riscaldamento',
+        'icon' => 'riscaldamento.svg'
+    ],
+    [
+        'name' => 'Camino',
+        'icon' => 'camino.svg'
+    ],
+    [
+        'name' => 'Cucina',
+        'icon' => 'cucina.svg'
+    ],
+    [
+        'name' => 'Colazione inclusa',
+        'icon' => 'colazione.svg'
+    ],
+    [
+        'name' => 'Lavatrice',
+        'icon' => 'lavatrice.svg'
+    ],
+    [
+        'name' => 'Asciugatrice',
+        'icon' => 'asciugatrice.svg'
+    ],
+    [
+        'name' => 'Ferro da stiro',
+        'icon' => 'ferro.svg'
+    ],
+    [
+        'name' => 'Asciugacapelli',
+        'icon' => 'asciugacapelli.svg'
+    ],
+    [
+        'name' => 'TV',
+        'icon' => 'tv.svg'
+    ],
+    [
+        'name' => 'Wi-Fi',
+        'icon' => 'wi-fi.svg'
+    ],
+    [
+        'name' => 'Rilevatore di fumo',
+        'icon' => 'fumo.svg'
+    ],
+    [
+        'name' => 'Culla',
+        'icon' => 'culla.svg'
+    ],
+    [
+        'name' => 'Animali ammessi',
+        'icon' => 'cane.svg',
+    ],
+    [
+        'name' => 'Posto auto',
+        'icon' => 'auto.svg',
+    ],
+    [
+        'name' => 'Vista mare',
+        'icon' => 'mare.svg',
+    ],
+    [
+        'name' => 'Vista montagna',
+        'icon' => 'montagna.svg',
+    ],
+    [
+        'name' => 'Vista lago',
+        'icon' => 'mare.svg',
+    ],
+    [
+        'name' => 'Kit cortesia',
+        'icon' => 'kit.svg',
+    ],
+    [
+        'name' => 'Cancellazione gratuita',
+        'icon' => 'calendario.svg',
+    ]
+];
