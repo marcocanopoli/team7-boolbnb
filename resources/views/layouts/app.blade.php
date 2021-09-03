32f69eb357bc56f2220ed6e116e2dac20cda8623
marcocanopoli <canopoli.marco@gmail.com>
2021-09-03 13:51:25 +0200

    upload-preview js + css, template css yield

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    @yield('css')
+    
 </head>
 <body>
     <div id="app">
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/upload_preview.js', 'public/js') 
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/upload_preview.scss', 'public/css')
+    .options({
+        processCssUrls: false
+    });
