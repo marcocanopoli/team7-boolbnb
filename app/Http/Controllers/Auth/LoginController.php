e319138cad47a8acbda48f408e448590944ab551
marcocanopoli <canopoli.marco@gmail.com>
2021-09-08 10:21:22 +0200

    backend dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function authenticated(Request $request)
+    {
+        $request->session()->flash('loggedIn', 'Login effettuato correttamente!');
+    }
+
+    // protected function loggedOut(Request $request)
+    // {
+    //     $request->session()->flash('loggedOut', 'A presto!');
+    // }
 }
